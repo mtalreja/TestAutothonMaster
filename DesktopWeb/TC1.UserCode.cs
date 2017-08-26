efd4f4d9fa09d6481ad00d81da40202ad0a08cb5
Mohit, Talreja <mtalreja2013@gmail.com>
2017-08-26 20:30:13 +0530

    BuildTriggerSecond

--- DesktopWeb/TC1.UserCode.cs
+++ DesktopWeb/TC1.UserCode.cs
@@ -47,6 +47,8 @@ namespace DesktopWeb
         	var lvComboBoxValue = WebSpecific.GetSelectedComboboxValue(By.Id(pId));  //Element Search
         	IWebElement elem = string.Empty;
         	
+        	
+        	
         	if(lvComboBoxValue.Equals(pExpectedCountry,StringComparison.InvariantCultureIgnoreCase))
         	{
         		Logger.logSnapshot(elem);
